adc5fea2ac83fc54c9a2f3212dff81bc1a76d92f
KKT84 <35786891+kthoma2484@users.noreply.github.com>
2018-06-16 09:25:04 -0700

    added logic to js file and audio

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,34 +1,35 @@
 $(document).ready(function() {
-/*
-let triviaQ = {
-        1: {Question: "What is the difference between red wine and white wine?", ans1: "White wine is made from different grapes", ans2: "Red wine is harvested later", ans3: "When making red wine, the skins of the grapes are left in the wine during fermentation", ans4: "The skins of red wine grapes are darker in color"
-        },
-        2: {Question: "What is the difference between looking at a French wine and an American, in terms of how it's labeled?", ans1: "French wine is more expensive.", ans2: "American wine marked with alcohol content", ans3: "American wine is more regulated in terms of what it can be called", ans4: "French wine is labeled by where it is made"
-        },
-        3: {Question: "Sangiovese is the type of grape used to make...", ans1: "Bordeaux style wine", ans2: "White table wine", ans3: "Chianti style wine", ans4: "Rose wine"
-        },
-        4: {Question: "Which of these white wines is usually a little sweet?", ans1: "Riesling", ans2: "Sauvignon Blanc", ans3: "Chardonnay", ans4: "Pinot Grigio"
-        },
-        5: {Question: "Which of these red wines is usually (most commonly) a little spicy or peppery?", ans1: "Cabernet Sauvignon", ans2: "Shiraz", ans3: "Merlot", ans4: "Pinot Noir"
-        },
-        6: {Question: "Why is champagne bubbly?", ans1: "The wine is aged less", ans2: "The wine undergoes a secondary fermentation, usually yeast and sugar is added", ans3: "The wine is aged in steel", ans4: "The wine is shaken"
-        },
-        7: {Question: "What is the traditional way to pour wine during a wine service?", ans1: "The person who ordered the wine is first, then clockwise around the table", ans2: "Counterclockwise", ans3: "Ladies first, then men, finishing with whomever ordered the bottle", ans4: "Ladies first, then men, no matter who ordered the bottle"
-        },
-        8: {Question: "What is a stelvin?", ans1: "A wine opener", ans2: "A special glass for port wine", ans3: "A type of wine cap, commonly known as a screw cap", ans4: "An expert on wine, like a sommelier"
-        },
-        9: {Question: "Why do people smell the cork during a wine service?", ans1: "To see if the wine has gone bad", ans2: "To see if the wine has been aged properly", ans3: "To get a sense of the wine", ans4: "None of the above"
-        },
-        10: {Question: "What are the grapes in a Bordeaux style wine?", ans1: "Cabernet sauvignon, pinot noir, cabernet franc", ans2: "Merlot, carmenere, petit verdot and cabernet sauvignon", ans3: "Merlot, malbec and pinot noir", ans4: "Merlot, cabernet sauvignon, cabernet franc, petit verdot, carmenere and malbec"
-        },
-}
-*/
 
-$('.start').click(function() {
+        let numRight = 0;
+        let numWrong = 0;
+        let notAns = 0;
+        let totAns = 0
+        let themeSong = document.getElementById('page-audio');
+                
+        $('.play-audio').click(function(){
+        themeSong.play();
+        });
 
-  
+        $('.pause-audio').click(function(){
+        themeSong.pause();
+        });
 
+        $('.start').click(function() {
+                $(".container").removeClass("hide-me1");
+                $(".start").addClass("hide-me1");
+        })
 
-})
+        $("input:radio").click(function() {
+                totAns++;
+                console.log(totAns);
+                numRight++;
+                console.log(numRight);     
+                        
+                
+        });
 
+        $('.done').click(function() {
+                $(".container").addClass("hide-me1");
+                $(".final-tally").removeClass("hide-me2");
+        });    
 });
