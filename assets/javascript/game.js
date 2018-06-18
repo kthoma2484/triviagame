8fac6ead1c8e73bccb54135984a797aedf7335e6
KKT84 <35786891+kthoma2484@users.noreply.github.com>
2018-06-17 23:32:27 -0700

    timer appears and disappears and added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+        let count = "";
+        let counter = "";
         let count1 = 0;
         let count2 = 0;
         let numRight = 0;
@@ -17,10 +19,41 @@ $(document).ready(function() {
         themeSong.pause();
         });
 
+        // timer function
+        function timer() {
+                console.log('timer ran mofo!');
+                count=count-1;
+                $('.timer').html(count + ' seconds');
+                if (count <= 0) {
+                    clearInterval(counter);
+                    $(".container").addClass("hide-me2");
+                    $(".final-tally").removeClass("hide-me2");
+                    
+                    // tally number of write questions
+                    let numRight = count2
+                    console.log(numRight)
+                    $("#correct").html(numRight);
+    
+                    // tally number of wrong questions  
+                    let numWrong = count1 - count2
+                    console.log(numWrong) 
+                    $("#wrong").html(numWrong);
+    
+                    // tally unanswered questions
+                    notAns = 10 - numWrong - numRight
+                    $("#no-answer").html(notAns);
+                    
+                }
+            }
+
         // click start button to...
         $('.start').click(function() {
                 event.preventDefault();
-                // show trivia questions
+                // start timer
+                count=60
+                counter=setInterval(timer, 1000);                
+                // show trivia questions and timer
+                $(".timer").removeClass("hide-me1");
                 $(".container").removeClass("hide-me1");
                 // hide start button
                 $(".start").addClass("hide-me1");
@@ -46,8 +79,9 @@ $(document).ready(function() {
         // click finish button to...
         $('.done').click(function() {
                 event.preventDefault(); 
-                // hide trivia questions and show finally tallies
+                // hide trivia questions/timer and show finally tallies
                 $(".container").addClass("hide-me2");
+                $(".timer").addClass("hide-me2");
                 $(".final-tally").removeClass("hide-me2");
                 
                 // tally number of write questions
